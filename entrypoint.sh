3a76220da7ae5ddbfe15fcfce4a3564d62df62a1
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-09-03 15:06:11 +0200

    add git

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,5 @@ export GIT_SHA_SHORT=$(git rev-parse --short HEAD)
 export CURRENT_DATE=$(date +%F-%T)
 export ELEMENTARY_VERSION="$CURRENT_DATE-$GIT_SHA_SHORT"
 sed -i 's/elementary_version/$ELEMENTARY_VERSION/g' k8s.yml
+echo "Deploying $ELEMENTARY_VERSION"
 sh -c "kubectl apply -f k8s.yml"
