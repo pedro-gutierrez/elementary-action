560470e548e00a917d81ca3646d1f2a816cd910a
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-09-03 15:12:27 +0200

    fix version replacement

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config
 export GIT_SHA_SHORT=$(git rev-parse --short HEAD)
 export CURRENT_DATE=$(date +%F-%T)
 export ELEMENTARY_VERSION="$CURRENT_DATE-$GIT_SHA_SHORT"
-sed -i 's/elementary_version/$ELEMENTARY_VERSION/g' k8s.yml
+sed -i "s/elementary_version/$ELEMENTARY_VERSION/g" k8s.yml
 echo "Deploying $ELEMENTARY_VERSION"
-sh -c "kubectl apply -f k8s.yml"
+kubectl apply -f k8s.yml
