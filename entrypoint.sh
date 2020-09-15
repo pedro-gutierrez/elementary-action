ff4aca19cf831935f51793cbcd740e4998419495
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-09-16 00:28:19 +0200

    more generic

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config
 
 export GIT_SHA_SHORT=$(git rev-parse --short HEAD)
 export CURRENT_DATE=$(date +%F-%T)
-export ELEMENTARY_VERSION="$CURRENT_DATE-$GIT_SHA_SHORT"
-sed -i "s/elementary_version/$ELEMENTARY_VERSION/g" k8s.yml
-echo "Deploying $ELEMENTARY_VERSION"
+export VERSION="$CURRENT_DATE-$GIT_SHA_SHORT"
+sed -i "s/{{VERSION}}/$VERSION/g" k8s.yml
+echo "Deploying $VERSION"
 kubectl apply -f k8s.yml
